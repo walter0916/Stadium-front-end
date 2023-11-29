0e81ad9d3846082456d02cf362ee87e8d56e3a3c
WalterGuerra <wsteven00@gmail.com>
2023-11-29 14:04:18 -0500

    add svgs for each league

--- src/components/LeagueCard/LeagueCard.jsx
+++ src/components/LeagueCard/LeagueCard.jsx
@@ -1,11 +1,55 @@
-import logo from '../../assets/laliga.svg'
+import laliga from '../../assets/laliga.svg'
+import premierLeagueLogo from '../../assets/premier-league.svg'
+import serieALogo from '../../assets/serieA.svg'
+import bundesligaLogo from '../../assets/bundesliga.svg'
+import mlsLogo from '../../assets/mls.svg'
+import ligue1Logo from '../../assets/ligue-1.svg'
+import europaLogo from '../../assets/europa.svg'
+import championsLogo from '../../assets/champions.svg'
 import styles from './LeagueCard.module.css'
 
-const LeagueCard = () => {
+const LeagueCard = (props) => {
+  console.log(props)
+  console.log('League Name:', props.interest.leagueName)
+  let logo = null
+
+  // Check if props.leagueName is defined before calling toLowerCase()
+  if (props.interest.leagueName && typeof props.interest.leagueName === 'string') {
+    switch (props.interest.leagueName.toLowerCase()) {
+      case 'laliga':
+        logo = laliga
+        break
+      case 'premier league':
+        logo = premierLeagueLogo
+        break
+      case 'serie a':
+        logo = serieALogo
+        break
+      case 'bundesliga':
+        logo = bundesligaLogo
+        break
+      case 'mls':
+        logo = mlsLogo
+        break
+      case 'ligue 1':
+        logo = ligue1Logo
+        break
+      case 'champions league':
+        logo = championsLogo
+        break
+      case 'europa league':
+        logo = europaLogo
+        break
+      default:
+        logo = laliga
+        break
+    }
+  }
+
+
   return (
-    <div>
+    <div className={styles.logoContainer}>
       <img src={logo} alt="" className={styles.logo}/>
-      
     </div>
   );
 }
