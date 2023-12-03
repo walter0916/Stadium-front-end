9ce140f9b951b63e774530c733bc620a85a1b36b
WalterGuerra <wsteven00@gmail.com>
2023-12-03 17:23:27 -0500

    setup sections for dashboard and style the dashboard as well as fix styling for navBar

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,18 @@
-// css
-import { useState, useEffect } from 'react'
-import styles from './Dashboard.module.css'
-import LeagueCard from '../../components/LeagueCard/LeagueCard'
-import * as leagueService from '../../services/leagueService'
-import * as profileService from '../../services/profileService'
-import SearchBar from '../../components/SearchBar/SearchBar'
-
-
+import { useState, useEffect } from 'react';
+import styles from './Dashboard.module.css';
+import LeagueCard from '../../components/LeagueCard/LeagueCard';
+import * as leagueService from '../../services/leagueService';
+import * as profileService from '../../services/profileService';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Landing = ({ user }) => {
   const [leagues, setLeagues] = useState([])
   const [profile, setProfile] = useState([])
+
+  library.add(fas)
 
   useEffect(() => {
     const fetchLeagues = async () => {
@@ -18,7 +20,7 @@ const Landing = ({ user }) => {
       const profileData = await profileService.getProfileById()
       setLeagues(data)
       setProfile(profileData)
-    }
+    };
     fetchLeagues()
   }, [])
 
@@ -27,17 +29,58 @@ const Landing = ({ user }) => {
 
   return (
     <main className={styles.container}>
-      <h1>{user ? user.name : 'friend'} Dashboard</h1>
+      <h1 className={styles.header}>{user ? `${user.name}'s` : "Friend's"} Dashboard</h1>
       <div className={styles.searchBarContainer}>
-      <SearchBar />
+        <SearchBar />
       </div>
       <div className={styles.interests}>
-      {profile.interests ? (
-        profile.interests.map(interest => <LeagueCard key={interest.id} interest={interest}/>)
+        {profile.interests ? (
+          profile.interests.map((interest) => (
+            <LeagueCard key={interest.id} interest={interest} />
+          ))
         ) : (
-        <p>No interests available</p>
-      )}
+          <p>No interests available</p>
+        )}
       </div>
+      <div className={styles.secondContainer}>
+      <div className={styles.communitiesNotificationContainer}>
+
+      <div className={styles.joinedCommunities}>
+        <h2>Joined Communities</h2>
+        {profile.joinedCommunities ? (
+          <ul className={styles.communitiesList}>
+            {profile.joinedCommunities.map((community) => (
+              <li key={community.id}>{community.teamName}</li>
+              ))}
+          </ul>
+        ) : (
+          <p>No communities joined</p>
+          )}
+      </div>
+      {/* New section: Notifications */}
+      <div className={styles.notifications}>
+        <h2>Notifications</h2>
+        <FontAwesomeIcon icon={['fas', 'bell']} className={styles.bell} />
+        <p>{/* Display notification count */}</p>
+        {/* Link to notifications page */}
+        <a href="/profile/notifications">See All</a>
+      </div>
+      {/* New section: Trending Blogs */}
+        </div>
+      <div className={styles.trendingBlogs}>
+        <h2>Trending Blogs</h2>
+        {/*
+          Render trending blogs based on likes, comments, and user interests
+        */}
+        {/* Sample blog */}
+        <div className={styles.trendingBlog}>
+          <p>Blog Title</p>
+          <p>Author: John Doe</p>
+          <p>Likes: 50</p>
+          <p>Comments: 30</p>
+        </div>
+      </div>
+    </div>
     </main>
   )
 }
