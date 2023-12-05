8e56d5352914e3d5d977ca5ed5efe33986a4cf0d
WalterGuerra <wsteven00@gmail.com>
2023-12-05 17:28:26 -0500

    edit create function to also add photo to blog

--- src/services/blogService.js
+++ src/services/blogService.js
@@ -3,20 +3,45 @@ import * as tokenService from './tokenService'
 
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/blogs`
 
-async function create(blogFormData) {
+async function create(postFormData, photoData) {
   try {
     const res = await fetch(BASE_URL, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${tokenService.getToken()}`,
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(blogFormData),
-
+      body: JSON.stringify(postFormData),
     })
-    return res.json()
+    const createdBlog = await res.json()
+    if (createdBlog._id) {
+      await addPhotoToBlog( createdBlog._id, photoData)
+    } else {
+      console.error('Failed to create blog')
+    }
+    return createdBlog
   } catch (error) {
     console.log(error)
+    throw new Error('Failed to create blog')
+  }
+}
+
+async function addPhotoToBlog(blogId, photoData) {
+  try {
+    const formData = new FormData()
+    formData.append('photo', photoData)
+    const res = await fetch(`${BASE_URL}/${blogId}/add-photo`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+      },
+      body: formData,
+    })
+    const updatedBlog = await res.json()
+    return updatedBlog
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to add photo to blog')
   }
 }
 
