d958afc895e35f69ac1774b829cb8adbe11a8e7c
WalterGuerra <wsteven00@gmail.com>
2023-12-07 12:13:49 -0500

    render blogs for league page and add styling to blogs

--- src/pages/League/League.jsx
+++ src/pages/League/League.jsx
@@ -1,11 +1,33 @@
 import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import * as blogService from '../../services/blogService'
+import styles from './league.module.css'
 
-const League = () => {
-  
+const League = (props) => {
+  const { leagueId } = useParams()
+  const [leagueBlogs, setLeagueBlogs] = useState({})
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const data = await blogService.getAllBlogs()
+      const filteredData = data.filter(blog => blog.league._id === leagueId)
+      setLeagueBlogs(filteredData)
+    }
+    fetchBlogs()
+  },[leagueId])
 
   return (
-    <div>
-      hi
+    <div className={styles.leagueContainer}>
+      <h1>La Liga</h1>
+      <div className={styles.leagueBlogCards}>
+        {leagueBlogs.length ? leagueBlogs.map((blog) => (<div key={blog._id} className={styles.leagueBlogCard}>
+          <img src={blog.photo} alt="" />
+          <div className={styles.articleContent}>
+          <h2>{blog.title}</h2>
+          <p>via {blog.author.name}</p>
+          </div>
+        </div>)) : ''}
+      </div>
     </div>
   )  
 }
