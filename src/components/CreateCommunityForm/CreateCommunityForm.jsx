7b19b0965080c1491e6821195b73223feee9143d
WalterGuerra <wsteven00@gmail.com>
2023-12-15 13:35:30 -0500

    add createCommunityForm

---
+++ src/components/CreateCommunityForm/CreateCommunityForm.jsx
@@ -0,0 +1,18 @@
+// npm module 
+import { useState } from "react"
+
+// services
+import  * as communityService from '../../services/communityService'
+
+// styles 
+import style from './CreateCommunityForm.module.css'
+
+const CreateCommunityForm = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CreateCommunityForm;
--- src/components/LeaveCommunitiesForm/LeaveCommunitiesForm.jsx
+++ src/components/LeaveCommunitiesForm/LeaveCommunitiesForm.jsx
@@ -1,11 +1,14 @@
 // npm modules
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
+
+// components 
+import SearchBar from "../SearchBar/SearchBar"
 
 // services 
 import * as communityService from '../../services/communityService'
 
 const LeaveCommunitiesForm = (props) => {
-const [joinedCommunities, setJoinedCommunities] = useState(props.profile.joinedCommunities);
+const [joinedCommunities, setJoinedCommunities] = useState(props.profile.joinedCommunities)
 
 const handleLeaveCommunities = async (communityId) => {
   await communityService.leaveCommunity(communityId)
@@ -14,14 +17,19 @@ const handleLeaveCommunities = async (communityId) => {
 console.log(props.profile)
   return (
     <div>
+      <h1>
+        Find communities to join 
+      </h1>
+      <SearchBar />
       <h1>Joined Communities</h1>
       {joinedCommunities.map((community) => 
         <div key={community._id}>
           <h1>{community.teamName}</h1>
           <button onClick={() => handleLeaveCommunities(community._id)}>Leave</button>
         </div>)}
+      <h3>Didn't find what you were looking for? create your own community</h3>
     </div>
   )
 }
 
-export default LeaveCommunitiesForm;
+export default LeaveCommunitiesForm
