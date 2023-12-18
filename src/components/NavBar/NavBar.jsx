6ef9dceb7f04d7e7f0b155baae2e6f5e2bfa2021
WalterGuerra <wsteven00@gmail.com>
2023-12-18 13:52:22 -0500

    edit styling for nav bar as well as fetch profilebyId to add profile image and add a dropdown

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,58 @@
 // npm modules
+import { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
+// styles
 import styles from'./NavBar.module.css'
 
+// services
+import * as profileService from '../../services/profileService'
+
 const NavBar = ({ user, handleLogout }) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+  const [ profile, setProfile] = useState({})
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen)
+  }
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const data = await profileService.getProfileById(user.profile)
+      setProfile(data)
+    }
+    fetchProfile()
+  }, [user])
+
+
   return (
-    <nav>
-      {user ?
-        <ul className={styles.links}>
-          <li>Welcome, {user.name}</li>
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/profile">My Profile</NavLink></li>
-          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
-        </ul>
-      :
+    <nav className={styles.navContainer}>
+      {user ? (
+        <div className={styles.loggedInContainer}>
+          <div className={styles.homeLink}>
+            <NavLink to="/">
+              <FontAwesomeIcon icon={faHome} />
+            </NavLink>
+          </div>
+          <div className={styles.userProfile} onClick={toggleDropdown}>
+            <p>Welcome, {user.name}</p>
+            <img src={profile.photo} alt="User Profile" />
+          </div>
+          {dropdownOpen && (
+            <ul className={styles.dropdown}>
+              <li><NavLink to="/profile">My Profile</NavLink></li>
+              <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
+              <li onClick={handleLogout}>Log Out</li>
+            </ul>
+          )}
+        </div>
+      ) : (
         <ul className={styles.links}>
           <li><NavLink to="/auth/login">Log In</NavLink></li>
           <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
         </ul>
-      }
+      )}
     </nav>
   )
 }
