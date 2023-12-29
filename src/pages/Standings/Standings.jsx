6e1ef5f87ebaac0655dd36e9a7b5a0535ae5d1a2
WalterGuerra <wsteven00@gmail.com>
2023-12-29 12:49:26 -0500

    add conditional rendering to check if standings are broken up into groups

--- src/pages/Standings/Standings.jsx
+++ src/pages/Standings/Standings.jsx
@@ -1,5 +1,5 @@
-// npm modules 
-import { useLocation } from "react-router-dom"
+// npm modules
+import { useLocation } from 'react-router-dom'
 
 // styles 
 import styles from './Standings.module.css'
@@ -9,36 +9,79 @@ const Standings = () => {
   const standingsData = location.state?.standings
   const flattenedStandings = standingsData.flat()
 
-  console.log(flattenedStandings)
+  if (flattenedStandings.length <= 30) {
+    return (
+      <div className={styles.standingsContainer}>
+        <table className={styles.standingsTable}>
+          <thead>
+            <tr>
+              <th className={styles.headerCell}>Team</th>
+              <th className={styles.headerCell}>Wins</th>
+              <th className={styles.headerCell}>Draws</th>
+              <th className={styles.headerCell}>Losses</th>
+              <th className={styles.headerCell}>Goals Scored</th>
+              <th className={styles.headerCell}>Goals Allowed</th>
+              <th className={styles.headerCell}>Goal Differential</th>
+            </tr>
+          </thead>
+          <tbody>
+            {flattenedStandings.map((standing) => (
+              <tr key={standing.team.id} className={styles.standingRow}>
+                <td className={styles.cell}>{standing.team.name}</td>
+                <td className={styles.cell}>{standing.all.win}</td>
+                <td className={styles.cell}>{standing.all.draw}</td>
+                <td className={styles.cell}>{standing.all.lose}</td>
+                <td className={styles.cell}>{standing.all.goals.for}</td>
+                <td className={styles.cell}>{standing.all.goals.against}</td>
+                <td className={styles.cell}>{standing.goalsDiff}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
+  const standingsByGroup = flattenedStandings.reduce((acc, standing) => {
+    const groupName = standing.group || 'Other'
+    acc[groupName] = acc[groupName] || []
+    acc[groupName].push(standing)
+    return acc
+  }, {})
 
   return (
-    <div className={styles.standingsContainer}>
-      <table className={styles.standingsTable}>
-        <thead>
-          <tr>
-            <th className={styles.headerCell}>Team Name</th>
-            <th className={styles.headerCell}>Wins</th>
-            <th className={styles.headerCell}>Draws</th>
-            <th className={styles.headerCell}>Losses</th>
-            <th className={styles.headerCell}>Goals Scored</th>
-            <th className={styles.headerCell}>Goals Against</th>
-            <th className={styles.headerCell}>Goal Differential</th>
-          </tr>
-        </thead>
-        <tbody>
-          {flattenedStandings.map((standing, index) => (
-            <tr key={index}>
-              <td className={styles.cell}>{standing.team.name}</td>
-              <td className={styles.cell}>{standing.all.win}</td>
-              <td className={styles.cell}>{standing.all.draw}</td>
-              <td className={styles.cell}>{standing.all.lose}</td>
-              <td className={styles.cell}>{standing.all.goals.for}</td>
-              <td className={styles.cell}>{standing.all.goals.against}</td>
-              <td className={styles.cell}>{standing.goalsDiff}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div>
+      {Object.entries(standingsByGroup).map(([groupName, groupStandings]) => (
+        <div key={groupName} className={styles.standingsContainer}>
+          <h2>{groupName}</h2>
+          <table className={styles.standingsTable}>
+            <thead>
+              <tr>
+                <th className={styles.headerCell}>Team</th>
+                <th className={styles.headerCell}>Wins</th>
+                <th className={styles.headerCell}>Draws</th>
+                <th className={styles.headerCell}>Losses</th>
+                <th className={styles.headerCell}>Goals Scored</th>
+                <th className={styles.headerCell}>Goals Allowed</th>
+                <th className={styles.headerCell}>Goal Differential</th>
+              </tr>
+            </thead>
+            <tbody>
+              {groupStandings.map((standing) => (
+                <tr key={standing.team.id} className={styles.standingRow}>
+                  <td className={styles.cell}>{standing.team.name}</td>
+                  <td className={styles.cell}>{standing.all.win}</td>
+                  <td className={styles.cell}>{standing.all.draw}</td>
+                  <td className={styles.cell}>{standing.all.lose}</td>
+                  <td className={styles.cell}>{standing.all.goals.for}</td>
+                  <td className={styles.cell}>{standing.all.goals.against}</td>
+                  <td className={styles.cell}>{standing.goalsDiff}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ))}
     </div>
   )
 }
