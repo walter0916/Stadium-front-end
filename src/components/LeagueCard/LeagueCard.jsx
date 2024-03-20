a371c0bcd3fbb79b0e06150d73bb6d83e501fba0
WalterGuerra <wsteven00@gmail.com>
2024-03-20 17:18:25 -0400

    edit league card to render proper image

--- src/components/LeagueCard/LeagueCard.jsx
+++ src/components/LeagueCard/LeagueCard.jsx
@@ -1,59 +1,15 @@
 // npm modules
 import { Link } from 'react-router-dom'
 
-// images
-import laliga from '../../assets/laliga.svg'
-import premierLeagueLogo from '../../assets/premier-league.svg'
-import serieALogo from '../../assets/serieA.svg'
-import bundesligaLogo from '../../assets/bundesliga.svg'
-import mlsLogo from '../../assets/mls.svg'
-import ligue1Logo from '../../assets/ligue-1.svg'
-import europaLogo from '../../assets/europa.svg'
-import championsLogo from '../../assets/champions.svg'
-
 // styles
 import styles from './LeagueCard.module.css'
 
 const LeagueCard = (props) => {
-  let logo = null
-
-  if (props.interest.leagueName && typeof props.interest.leagueName === 'string') {
-    switch (props.interest.leagueName.toLowerCase()) {
-      case 'laliga':
-        logo = laliga
-        break
-      case 'premier league':
-        logo = premierLeagueLogo
-        break
-      case 'serie a':
-        logo = serieALogo
-        break
-      case 'bundesliga':
-        logo = bundesligaLogo
-        break
-      case 'mls':
-        logo = mlsLogo
-        break
-      case 'ligue 1':
-        logo = ligue1Logo
-        break
-      case 'champions league':
-        logo = championsLogo
-        break
-      case 'europa league':
-        logo = europaLogo
-        break
-      default:
-        logo = laliga
-        break
-    }
-  }
-
 
   return (
     <div className={styles.logoContainer}>
       <Link to={`/league/${props.interest._id}`}>
-      <img src={logo} alt="" className={styles.logo}/>  
+      <img src={props.interest.logo} alt="" className={styles.logo}/>  
       </Link>
     </div>
   )
