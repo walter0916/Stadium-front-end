ddc99f2a136304c030336a959a4631c2b605f4ce
WalterGuerra <wsteven00@gmail.com>
2024-03-23 12:50:08 -0400

    edit link for player info page on dashboard to pass correct ids to match route and add useEffect to playerInfo page to fetch player info

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -89,7 +89,7 @@ const Dashboard = ({ user }) => {
             {profile.favoritePlayers.map((player) => (
               <li key={player._id}>
                 <div>
-                  <Link to={`/player/${player.teamId}/${player.playerId}/statistics`}>
+                  <Link to={`/player/${player.playerId}/${player.teamId}/statistics`}>
                     <img  src={player.photo} alt={player.name} className={styles.circle} />
                   </Link>
                 </div>
--- src/pages/PlayerInfo/PlayerInfo.jsx
+++ src/pages/PlayerInfo/PlayerInfo.jsx
@@ -1,7 +1,27 @@
+// npm services
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+
+// services 
+import * as profileService from '../../services/profileService'
+
 const PlayerInfo = () => {
+  const { playerId, teamId } = useParams()
+  const [playerInfo, setPlayerInfo] = useState([])
+  console.log(playerId)
+
+  useEffect(() => {
+    const fetchPlayerInfo = async () => {
+      const data = await profileService.getPlayerInfoById(playerId, teamId, 2023)
+      const filteredData = data.response
+      setPlayerInfo(filteredData)
+    }
+    fetchPlayerInfo()
+  },[playerId, teamId])
+
   return (
     <div>
-      
+
     </div>
   )
 }
