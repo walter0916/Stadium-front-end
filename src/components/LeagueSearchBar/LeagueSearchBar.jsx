5fbaa41f4ab226cf01067a3ac0245d5c683b4c2f
WalterGuerra <wsteven00@gmail.com>
2024-03-24 14:35:17 -0400

    fix styles import for league search bar

--- src/components/LeagueSearchBar/LeagueSearchBar.jsx
+++ src/components/LeagueSearchBar/LeagueSearchBar.jsx
@@ -6,7 +6,7 @@ import * as leagueService from '../../services/leagueService'
 import * as profileService from '../../services/profileService'
 
 // styles
-import styles from './leagueSearchBar.module.css'
+import styles from './leagueSearchBarStyles.module.css'
 
 const LeagueSearchBar = () => {
   const [searchResults, setSearchResults] = useState([])
