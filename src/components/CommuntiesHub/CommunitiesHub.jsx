31af9d2cc76e74a5a6e6e0b4d98c2013164274c6
WalterGuerra <wsteven00@gmail.com>
2024-03-24 14:09:41 -0400

    render player info and stats in playerinfo page as well as add styling and edit communities hub page to remove create community and community search cmponent as well as add link to community page and edit styling

--- src/components/CommuntiesHub/CommunitiesHub.jsx
+++ src/components/CommuntiesHub/CommunitiesHub.jsx
@@ -1,5 +1,6 @@
 // npm modules
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
 // components
 import SearchBar from "../TeamSearchBar/TeamSearchBar"
@@ -31,17 +32,17 @@ const CommunitiesHub = (props) => {
   return (
     <div className={styles.communitiesHub}>
       <div className={styles.joinedSearchContainer}>
-      <section className={styles.searchSection}>
+      {/* <section className={styles.searchSection}>
         <h1>Find communities to join</h1>
         <SearchBar />
-      </section>
+      </section> */}
       <section className={styles.joinedCommunitiesSection}>
         <h1>Joined Communities</h1>
         {loading && <p>Loading...</p>}
         {error && <p className={styles.error}>{error}</p>}
         {joinedCommunities.map((community) => (
           <div key={community._id} className={styles.communityCard}>
-            <p>{community.teamName}</p>
+            <Link to={`/community/${community._id}`}><img src={community.logo} alt="" />{community.teamName}</Link>
             <button onClick={() => handleLeaveCommunity(community._id)} disabled={loading}>
               {loading ? 'Leaving...' : 'Leave'}
             </button>
@@ -49,11 +50,6 @@ const CommunitiesHub = (props) => {
         ))}
       </section>
       </div>
-
-      <section className={styles.createCommunitySection}>
-        <h3>Didn't find what you were looking for? Create your own community</h3>
-        <CreateCommunityForm />
-      </section>
     </div>
   )
 }
--- src/pages/PlayerInfo/PlayerInfo.jsx
+++ src/pages/PlayerInfo/PlayerInfo.jsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from "react"
 // services 
 import * as profileService from '../../services/profileService'
 
+import styles from './PlayerInfo.module.css'
+
 const PlayerInfo = () => {
   const { playerId, teamId } = useParams()
   const [playerInfo, setPlayerInfo] = useState([])
@@ -20,8 +22,53 @@ const PlayerInfo = () => {
   },[playerId, teamId])
 
   return (
-    <div>
-
+    <div className={styles.mainContainer}>
+      {playerInfo.length > 0 ? (
+        (playerInfo.map( player => 
+          <div key={player.player.id} className={styles.PlayerInfo}>
+            <h2>{player.player.name}</h2>
+            <img src={player.player.photo} alt="player photo" className={styles.playerPhoto} />
+            <p className={styles.team}>
+              <img src={player.statistics[0].team.logo} alt="team photo" className={styles.teamPhoto}/>
+              <span>{player.statistics[0].team.name}</span> 
+            </p>
+            <p>Age: <span>{player.player.age}</span></p>
+            <p>Nationality: <span>{player.player.nationality}</span></p>
+            <p>Height: <span>{player.player.height}</span></p>
+            <p>Weight: <span>{player.player.weight}</span></p>
+            <p>Injured: <span>{player.player.injured ? ('Injured') : ('Not injured')}</span></p>
+          </div>
+        ))
+      ) : (
+        'no player info found'
+      )}
+      <div className={styles.statCardsContainer}>
+      {playerInfo.length > 0 ? (
+        (playerInfo.map( player =>
+          (player.statistics.map( statistic => 
+            <div key={player.player.id} className={styles.statsCard}>
+              <h2>{statistic.league.name}</h2>
+              <p>Appearences: <span className={styles.statValue}>{statistic.games.appearences}</span></p>
+              <p>Minutes played: <span className={styles.statValue}>{statistic.games.minutes}</span></p>
+              <p>Average rating: <span className={styles.statValue}>{statistic.games.rating}</span></p>
+              <p>Total shots: <span className={styles.statValue}>{statistic.shots.total}</span></p>
+              <p>Shots on target: <span className={styles.statValue}>{statistic.shots.on}</span></p>
+              <p>Goals: <span className={styles.statValue}>{statistic.goals.total}</span></p>
+              <p>Assists: <span className={styles.statValue}>{statistic.goals.assists}</span></p>
+              <p>Dribbles attempted: <span className={styles.statValue}>{statistic.dribbles.attempts}</span></p>
+              <p>Dribbles succeeded: <span className={styles.statValue}>{statistic.dribbles.success}</span></p>
+              <p>Key Passes: <span className={styles.statValue}>{statistic.passes.key}</span></p>
+              <p>Tackles: <span className={styles.statValue}>{statistic.tackles.total}</span></p>
+              <p>Fouls Drawn: <span className={styles.statValue}>{statistic.fouls.drawn}</span></p>
+              <p>Yellow Cards: <span className={styles.statValue}>{statistic.cards.yellow}</span></p>
+              <p>Red Cards: <span className={styles.statValue}>{statistic.cards.red}</span></p>
+            </div>
+          ))
+        ))
+      ) : (
+        'no player stats found'
+      )}
+      </div>
     </div>
   )
 }
